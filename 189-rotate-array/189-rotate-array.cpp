4e5694bee2fbb31d5ca84127087636b75fc69858
LakshKumar664 <107551559+LakshKumar664@users.noreply.github.com>
2022-09-25 04:05:00 +0500

    Time: 57 ms (42.99%), Space: 25.1 MB (59.10%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        /*
+            k = 9    2
+            
+        */
+        
+        int temp = 0;
+        int count = 0;
+        int idx = 0;
+        k = k % nums.size();
+        int prev_elem = nums[0];
+        
+        while(k && count < nums.size()){
+            count ++;
+            int new_idx = (idx + k) % nums.size();
+            temp = nums[new_idx];
+            nums[new_idx] = prev_elem;
+            long long num = count;
+            num = num * k;
+            if(((num) % nums.size() == 0 || nums.size() % k == 0) && idx + k >= nums.size()){
+                new_idx ++;
+                temp = nums[new_idx];
+                // nums[new_idx] = prev_elem;
+            }
+            idx = new_idx;
+            prev_elem = temp;
+        }
+        
+        
+    }
+};
